d718399c3b0de474b73671775a6e48e766711a37
robcabobb <robcabobb@gmail.com>
2022-01-16 23:19:48 -0500

    initial js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+citySearchEl = document.querySelector('.city-search')
+searchEl = document.querySelector('#search-btn')
+
+
+// id="current-conditions">
+// <h1 id="city"></h1>
+// <p id="temperature"></p>
+// <p id="wind-speed"></p>
+// <p id="humidity-index"></p>
+// <p id="uv-index"></p>
